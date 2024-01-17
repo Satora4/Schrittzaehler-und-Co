5d635d8c47986e404cbd490b3cf6535281c558c6
Joel Vontobel <joel.vontobel@ergon.ch>
2024-01-17 14:54:59 +0100

    initial commit

---
+++ app/src/main/java/com/m335/schrittzaehler_und_co/MainActivity.java
@@ -0,0 +1,14 @@
+package com.m335.schrittzaehler_und_co;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
